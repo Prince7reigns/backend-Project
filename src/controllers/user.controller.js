ab3d881ce8c49589d555bd4adfbf1847b557579b
Prince Yadav <prinsyadac@gmail.com>
2025-03-03 23:29:00 +0530

    add login user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,23 @@ import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { upload } from "../middlewares/multer.middlewares.js";
 
+const generateAccessAndRefereshTokens = async (userId) =>{
+    try {
+    const user = await User.findById(userId);
+
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+     
+    user.refreshToken = refreshToken
+
+    user.save({validateBeforeSave:false})
+
+    return {accessToken,refreshToken}
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async (req,res)=>{
      // get user details from frontend
     // validation - not empty
@@ -82,4 +99,60 @@ const registerUser = asyncHandler(async (req,res)=>{
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler( async (req,res) =>{
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+    const {email,password,username} = req.body
+
+    if(!email || !username){
+        throw new ApiError(400,"Email or username are required")
+    }
+
+    const user = User.findOne({
+        $or: [{ email }, { username }]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid password")
+    }
+
+    const {accessToken,refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id)
+    .select("-password -refreshToken")
+    
+    const options = {
+        httpOnly: true,
+        secure:true
+        // by defualt any one can change cooke from fronted when use true the httponly and secure then its only changeable from server
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+})
+
+export {
+    registerUser,
+    loginUser
+  }
