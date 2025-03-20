eca66525c526b9f8c8ce383d0dfc603ec4df2eb8
Prince Yadav <prinsyadac@gmail.com>
2025-03-20 23:21:26 +0530

    tweets model

---
+++ src/models/tweets.model.js
@@ -0,0 +1,18 @@
+import mongoose , {Schema} from "mongoose";
+
+
+const tweetsSchema = new Schema({
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    content:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+
+export const Tweet = mongoose.models("tweet",tweetsSchema)
