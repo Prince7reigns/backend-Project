472bf6858100d1e142e1895e5a2e4765d392ac3b
Prince Yadav <prinsyadac@gmail.com>
2025-03-20 23:25:54 +0530

    comments model

---
+++ src/models/comments.models.js
@@ -0,0 +1,21 @@
+ import mongoose,{Schema} from "mongoose"
+
+ const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    video:{
+        type: Schema.Types.ObjectId,
+        ref: 'Video',
+    }
+ },{
+    timestamps:true
+ })
+
+
+export const Comment = mongoose.models("comment" , commentSchema)
