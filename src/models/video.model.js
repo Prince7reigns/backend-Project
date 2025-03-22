c907013220600dc7ac4f58016278a25ad04f4640
Prince Yadav <prinsyadac@gmail.com>
2025-03-22 14:27:16 +0530

    `Removed updateUserCoverImage from user.routes.js`

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -3,34 +3,45 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 
 const videoSchema = new Schema({
-    videoFile:{
-        type: String,
-        required: [true, "videoFile is required"],
+    videoFile: {
+        type:{
+            url:String,
+            public_id:String
+        }, //cloudinary url
+        required: true
     },
-    thumbnail:{
-        type: String,
-        required: [true, "thumbnail is required"],
+    thumbnail: {
+        type:{
+            url:String,
+            public_id:String
+        }, //cloudinary url
+        required: true
     },
-    title:{
-        type: String,
-        required: [true, "title is required"],
+    title: {
+        type: String, 
+        required: true
     },
-    description:{
-        type: String,
-        required: [true, "Description is required"],
+    description: {
+        type: String, 
+        required: true
     },
-    duration:{
+    duration: {
+        type: Number, 
+        required: true
+    },
+    views: {
         type: Number,
+        default: 0
     },
-    videoFile:{
-        type: Number,
-        default:0
+    isPublished: {
+        type: Boolean,
+        default: true
     },
-    owner:{
+    owner: {
         type: Schema.Types.ObjectId,
-        ref: "User",
+        ref: "User"
     }
-    },
+},
     {
       timestamps: true
     }
