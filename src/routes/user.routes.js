9eb56e2b8523c0b69042dce6b91aee37f98cbb43
Prince Yadav <prinsyadac@gmail.com>
2025-03-22 14:28:19 +0530

    routes changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,6 @@ import {
     changeCurrentPassword, 
     getCurrentUser, 
     updateUserAvatar, 
-    updateUserCoverImage, 
     getUserChannelProfile, 
     getWatchHistory, 
     updateAccountDetails
@@ -29,6 +28,7 @@ router.route("/register").post(
     registerUser
 )
 
+
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
@@ -38,7 +38,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+//router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
