087593e9d89739e9ffdb772a17dc418c8cecca9c
Prince Yadav <prinsyadac@gmail.com>
2025-03-22 14:26:26 +0530

    `Refactor video model and remove user routes for updating cover image`

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,8 +1,10 @@
 import asyncHandler from "../utils/asyncHandler.js"
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
+    
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         
@@ -11,8 +13,7 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
             throw new ApiError(401, "Unauthorized request")
         }
     
-        const decodedToken = jwt.verify(token, )
-        console.log(decodedToken)
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
         
     
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
