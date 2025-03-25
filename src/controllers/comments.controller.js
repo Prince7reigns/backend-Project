9cd2049f707630ef6f56bfa01c8e5907ffb1e1ba
Prince Yadav <prinsyadac@gmail.com>
2025-03-25 09:56:17 +0530

    `Added get Video Comments and comments creation controller `

---
+++ src/controllers/comments.controller.js
@@ -0,0 +1,141 @@
+import mongoose from "mongoose"
+import {Comment} from "../models/comment.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {Video} from "../models/video.model.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+    if(!mongoose.Types.ObjectId(videoId)){
+        throw new ApiError(400, 'Invalid videoId')
+    }
+
+    const video = await Video.findById(videoId)
+
+    if(!mongoose.Types.ObjectId.isValid){
+        throw new ApiError(404, 'Video not found')
+    }
+
+    const commentsAggregate = await Comment.aggregate([
+        {
+            $match:{
+                video:mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"user",
+                foreignField:"_id",
+                as:"owner"
+            }
+        },
+        {
+            $lookup:{
+                from:"likes",
+                localField:"_id",
+                foreignField:"comment",
+                as:"likes"
+            }
+        },
+        {
+            $addFeilds:{
+                likeCount:{
+                    $size:"$likes"
+                },
+                owner:{
+                    $first:"$owner"
+                },
+                isLiked:{
+                    $cond:{
+                        if:{
+                            $in:[req.user._id,"$likes.likedBy"],
+                        },
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $sort:{
+                createdAt:-1
+            }
+        },
+        {
+            $project:{
+                content:1,
+                createdAt:1,
+                likeCount:1,
+                isLiked:1,
+                owner:{
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1
+                }
+            }
+        }
+    ]) 
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10)
+    };
+
+    const comments = Comment.aggregatePaginate(commentsAggregate,options)
+
+    return res 
+    .status(200)
+    .json(
+        new ApiResponse(200,comments,"comments fetched successfully")
+    )
+})
+
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+    const {videoId} = req.params;
+    const {content} = req.body;
+
+    if(!content){
+        throw new ApiError(400,"content is required")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if(!video){
+        throw new ApiError(404,"video not found")
+    }
+
+    const comment = await Comment.create({
+        content,
+        owner: req.user._id,
+        video: video._id
+    })
+
+    if(!comment){
+        throw new ApiError(500,"failed to create comment")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,comment,"comment added successfully"))
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+     deleteComment
+    }
