028d21c82c84f7456dcfcff7fa0862a6d2474c4f
Prince Yadav <prinsyadac@gmail.com>
2025-03-26 21:29:16 +0530

    add video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,25 +1,40 @@
 import { Router } from "express";
 import {upload}  from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js"
-import { getAllVideos, publishAVideo } from "../controllers/video.controller.js";
+import { 
+    deleteVideo,
+    getAllVideos,
+    getVideoById,
+    publishAVideo,
+    togglePublishStatus,
+    updateVideo,
+} from "../controllers/video.controller.js";
 
 const router = Router()
 
+router.use(verifyJWT);
 
-
-router.route("/publish-video")
-.get(verifyJWT,getAllVideos)
-.post(verifyJWT , upload.fields(
+router.route("/")
+.get(getAllVideos)
+.post(upload.fields(
     [{
         name:"videoFile",
         maxCount:1
     },{
         name:"thumbnail",
-        maxCount:1
+        maxCount:1       
     }]
   ),
   publishAVideo
 )
+
+router
+    .route("/:videoId")
+    .get(verifyJWT,getVideoById)
+    .delete(verifyJWT,deleteVideo)
+    .patch(verifyJWT , upload.single("thumbnail"), updateVideo);
+
+    router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
 
 
 export default router
